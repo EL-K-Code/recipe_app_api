4afa9c78768fbbd97a5e1ea01155f85e3f19ceb6
KOmla <alexlabou03@gmail.com>
2025-05-19 02:02:29 +0200

    Create User APi

---
+++ app/user/serializers.py
@@ -0,0 +1,47 @@
+"""Serializers for the User API"""
+
+from django.contrib.auth import (
+    get_user_model, authenticate,
+    )
+from django.utils.translation import gettext as _
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta :
+
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        extra_kwargs = {'password' : {'write_only':True, 'min_length': 5}}
+
+    def create(self, validated_data):
+
+        return get_user_model().objects.create_user(**validated_data)
+    
+class AuthTokenSerializer(serializers.Serializer):
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style = {'input_type':'password'},
+        trim_white_space = False,
+    )
+    
+    def validate(self, attrs):
+        """Validate and Authentificated the user"""
+
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username = email,
+            password = password
+        )
+
+        if not user :
+            msg = _('Unable to authentificate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+        
+        attrs['user'] = user
+        return attrs
+    
